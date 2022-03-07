2c884d2e3a222b266438d6907aa29b190442ed9a
Yelk11 <neighbour.matthew@gmail.com>
2022-03-07 13:10:08 -0500

    update

--- collector.py
+++ collector.py
@@ -1,42 +1,53 @@
 from bs4 import BeautifulSoup
 import re
 from urllib.parse import urljoin
+from pyparsing import col
+import spacy
+
+'''
+https://spacy.io
+https://spacy.io/api/entityrecognizer
+
+use entity recognition
+
+
+'''
+
 
 # TODO: implement Title extractor
-class Title_Extractor():
-    def __init__(self, soup:BeautifulSoup) -> None:
-        self.my_soup = soup
+class Collector():
+    def __init__(self, string:str, soup:BeautifulSoup) -> None:
+        self.input_str = string
+        self.soup = soup
+        self.nlp = spacy.load("en_core_web_sm") 
+        self.doc = self.nlp(self.input_str)
+        
     
-    def extract(self):
+    def get_title(self) -> str:
+        for token in self.doc[:10]:
+            print(token)
+            print(token.ent_type_)
+        return self.input_str
+    
+    def get_deadline(self) -> str:
         pass
-
-    def get_title(self):
+    
+    def get_organization(self) -> str:
         pass
-# TODO implement Deadline Extractor
-class Deadline_Extractor():
-    def __init__(self) -> None:
-        self.deadline = ''
-    def extract(self):
-        pass
-# TODO implement org extractor
-class Organization_Extractor:
-    def __init__(self, soup:BeautifulSoup) -> None:
-        self.soup = soup
-
-    def extract():
-        pass
-# TODO implement amount extractor
-class Amount_Extractor():
-    def __init__(self, soup:BeautifulSoup) -> None:
-        self.soup = soup
+    
+    def get_amount(self) -> int:
         self.amount = 0
-    
-    def extract(self) -> int:
         amount_list = self.soup.find_all(['strong','p', 'h1', 'h2', 'h3', 'span', 'b'])
         results = re.search('\$\d+(,|\d)\d+', str(amount_list))
         if results:
             return results.group(0)
         return self.amount
+    
+    def get_requirements(self):
+        pass
+
+
+        
 
 # TODO handle /index.html type urls by adding appening to base url
 class URL_Extractor():
@@ -63,12 +74,26 @@ class URL_Extractor():
     
         
 
+if __name__ == '__main__':
+    from loader import Page_Loader
+    schol_list = [
+        # 'https://www.nitrocollege.com/nitro-scholarship-application?utm_source=cpc&utm_medium=studentscholarships&utm_campaign=studentscholarships.org_student_2K',
+        # 'https://www.niche.com/colleges/scholarships/no-essay-scholarship/?utm_source=Fastweb&utm_medium=Referral&utm_campaign=FWnes',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/154428-albert-b-allison-memorial-endowed-scholarship',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/103541-church-vocation-grant-emory-henry-college',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/103543-music-and-theater-scholarship-emory-and-henry-college',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/103544-pre-med-scholarship-emory-henry-college',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/108168-james-and-nancy-johnson-scholarship-in-the-college-of-engineering',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/108169-union-contractors-of-agc-minority-scholarship',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/108173-rob-johnson-memorial-scholarship',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/108190-jennings-coe-family-scholarship',
+        # 'https://www.fastweb.com/college-scholarships/scholarships/113860-adam-andrews-memorial-scholarship',
+        'https://www.fastweb.com/college-scholarships/scholarships/113861-athletic-scholarship-howard-college'
 
+    ]
 
-# TODO implement req extractor
-class Requirement_Extractor():
-    def __init__(self, soup:BeautifulSoup) -> None:
-        self.requirements = []
-    
-    def extract(self):
-        pass
+    for url in schol_list:
+        loader = Page_Loader(url)
+        collector = Collector(loader.get_text(),loader.get_soup())
+        collector.get_title()
+        
--- validator.py
+++ validator.py
@@ -7,6 +7,11 @@ import numpy as np
 
 # Helpful paper
 # https://luca.ntop.org/LargeScaleWebClassification.pdf
+'''
+create a distrobution of sholarship data
+then look to see if there is a cutoff point between lists of scholarships and a single one
+as well as non-scholarship like pages
+'''
 
 
 
@@ -42,25 +47,25 @@ class Validator():
 if __name__ == '__main__':
     from loader import Page_Loader
     schol_list = [
-        # 'https://www.nitrocollege.com/nitro-scholarship-application?utm_source=cpc&utm_medium=studentscholarships&utm_campaign=studentscholarships.org_student_2K',
-        # 'https://www.niche.com/colleges/scholarships/no-essay-scholarship/?utm_source=Fastweb&utm_medium=Referral&utm_campaign=FWnes',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/154428-albert-b-allison-memorial-endowed-scholarship',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/103541-church-vocation-grant-emory-henry-college',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/103543-music-and-theater-scholarship-emory-and-henry-college',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/103544-pre-med-scholarship-emory-henry-college',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/108168-james-and-nancy-johnson-scholarship-in-the-college-of-engineering',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/108169-union-contractors-of-agc-minority-scholarship',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/108173-rob-johnson-memorial-scholarship',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/108190-jennings-coe-family-scholarship',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/113860-adam-andrews-memorial-scholarship',
-        # 'https://www.fastweb.com/college-scholarships/scholarships/113861-athletic-scholarship-howard-college'
+        'https://www.nitrocollege.com/nitro-scholarship-application?utm_source=cpc&utm_medium=studentscholarships&utm_campaign=studentscholarships.org_student_2K',
+        'https://www.niche.com/colleges/scholarships/no-essay-scholarship/?utm_source=Fastweb&utm_medium=Referral&utm_campaign=FWnes',
+        'https://www.fastweb.com/college-scholarships/scholarships/154428-albert-b-allison-memorial-endowed-scholarship',
+        'https://www.fastweb.com/college-scholarships/scholarships/103541-church-vocation-grant-emory-henry-college',
+        'https://www.fastweb.com/college-scholarships/scholarships/103543-music-and-theater-scholarship-emory-and-henry-college',
+        'https://www.fastweb.com/college-scholarships/scholarships/103544-pre-med-scholarship-emory-henry-college',
+        'https://www.fastweb.com/college-scholarships/scholarships/108168-james-and-nancy-johnson-scholarship-in-the-college-of-engineering',
+        'https://www.fastweb.com/college-scholarships/scholarships/108169-union-contractors-of-agc-minority-scholarship',
+        'https://www.fastweb.com/college-scholarships/scholarships/108173-rob-johnson-memorial-scholarship',
+        'https://www.fastweb.com/college-scholarships/scholarships/108190-jennings-coe-family-scholarship',
+        'https://www.fastweb.com/college-scholarships/scholarships/113860-adam-andrews-memorial-scholarship',
+        'https://www.fastweb.com/college-scholarships/scholarships/113861-athletic-scholarship-howard-college'
 
     ]
     mult_schol_list = [
-        'https://finaid.org/scholarships/',
-        'https://studentscholarships.org',
-        'https://signup.collegeboard.org/scholarship-search/',
-        'https://bold.org/scholarships/easy-scholarships-list/',
+        # 'https://finaid.org/scholarships/',
+        # 'https://studentscholarships.org',
+        # 'https://signup.collegeboard.org/scholarship-search/',
+        # 'https://bold.org/scholarships/easy-scholarships-list/',
         'https://www.fastweb.com',
         'https://www.fastweb.com/college-scholarships/articles/scholarships-for-average-students'
     ]
